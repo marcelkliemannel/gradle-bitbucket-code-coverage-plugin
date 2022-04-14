305151278cfcdf16f318e4f9f936a2f0659b2ce4
Marcel Kliemannel <dev@marcelkliemannel.com>
2022-04-14 21:47:30 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+  java
+  `java-gradle-plugin`
+  kotlin("jvm") version "1.6.20"
+  `maven-publish`
+}
+
+group = "dev.turingcomplete"
+version = "1.0.0"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  api(gradleApi())
+
+  val jUnitVersion = "5.7.1"
+  testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:$jUnitVersion")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+  testImplementation("org.jetbrains:annotations:23.0.0")
+}
+
+tasks.getByName<Test>("test") {
+  useJUnitPlatform()
+}
+
+gradlePlugin {
+  plugins {
+    create("bitbucketCodeCoverage") {
+      id = "dev.turingcomplete.bitbucket-code-coverage"
+      implementationClass = "dev.turingcomplete.bitbucketcodecoverage.BitbucketCodeCoveragePlugin"
+    }
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "bitbucket-code-coverage"
+
